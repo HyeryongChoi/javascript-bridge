8c7d986d2186d1418feb9c0fe83b58cd7f8ff401
chex <chex417@naver.com>
2022-12-12 21:50:55 +0900

    feat: 다리 길이를 입력받는 기능 추가

--- src/controllers/BridgeGameController.js
+++ src/controllers/BridgeGameController.js
@@ -1,11 +1,17 @@
 const BridgeGame = require('../models/BridgeGame');
 const { printGameStart } = require('../views/OutputView');
+const { readBridgeSize } = require('../views/InputView');
 
 class BridgeGameController {
   #bridgeGame;
 
   playGame() {
     printGameStart();
+    readBridgeSize(this.onReadBridgeSize.bind(this));
+  }
+
+  onReadBridgeSize(bridgeSize) {
+    //this.#bridgeGame = new BridgeGame(bridgeSize);
   }
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,13 @@
+const INPUT_MESSAGE = Object.freeze({
+  enterBridgeSize: '다리의 길이를 입력해주세요.\n',
+});
+
 const OUTPUT_MESSAGE = Object.freeze({
   newLine: '',
   gameStart: '다리 건너기 게임을 시작합니다.\n',
 });
 
 module.exports = {
+  INPUT_MESSAGE,
   OUTPUT_MESSAGE,
 };
